a4917a31391c90e5988c0aa41bfa0de63c8948a1
EC2 Default User <ec2-user@ip-172-31-13-33.us-east-2.compute.internal>
2021-08-05 04:36:07 +0000

    test

--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -1,28 +1,44 @@
-require "bowling"
-
-describe "bowlingのスコア計算" do
+require "Bowling"
+describe "ボウリングのスコア計算" do
+    # インスタンスの生成を共通化
+    before do
+        @game = Bowling.new
+    end
     describe"全体の合計" do
         context "すべての投球がガターだった場合" do
             it "0になること" do
-                @game = Bowling.new
-                
-                20.times do
-                    @game.add_score(0)
-                end
-                
+                add_many_scores(20,0)
                 expect(@game.total_score).to eq 0
             end
-end
+        end
+#                @game = Bowling.new
+#                20.times do
+#                   @game.add_score(0)
+#              end
+#                expect(@game.total_score).to eq 0
+#           end
+#       end
     context"すべての投球で1ビンずつ倒した場合" do
         it "20になること" do
-            @game = Bowling.new
-            
-            20.times do
-                @game.add_score(1)
-            end
-            
+            add_many_scores(20,1)
             expect(@game.total_score).to eq 20
         end
     end
 end
 end
+private
+# 複数回のスコア追加をまとめて実行する
+def add_many_scores(count,pins)
+    cont.times do
+        @game.add_score(pins)
+    end
+end
+#            @game = Bowling.new
+#            20.times do
+#                @game.add_score(1)
+#            end
+#            expect(@game.total_score).to eq 20
+#        end
+#    end
+#end
+#end
