188406bef1168532afb143199b7e27b25ee38cbe
EC2 Default User <ec2-user@ip-172-31-13-33.us-east-2.compute.internal>
2021-08-06 05:54:05 +0000

    xxx

--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -146,6 +146,32 @@ describe "全体の合計" do
              expect(@game.total_score).to eq 38
          end
      end
+     describe "フレーム事の合計" do
+         context "全ての投球で1ピンずつ倒した場合" do
+             it "1フレーム目の合計が2になる事" do
+                 add_many_scores(20, 1)
+                 #合計を計算
+                 @game.calc_score
+                 expect(@game.frame_score(1)).to eq 2
+             end
+         end
+         context "スペアを取った場合" do
+             it "スペアボーナスが加算されること" do
+                 #第一フレームで3点,7点のスペア
+                 @game.add_score(3)
+                 @game.add_score(7)
+                 #第二フレームの一投目で4点
+                 @game.add_score(4)
+                 #以降は全てガター
+                 add_many_scores(17, 0)
+                 #合計を計算
+                 @game.calc_score
+                 #期待する合計 *()内はボーナス点
+                 #3 + 7 +(4) = 14
+                 expect(@game.frame_score(1)).to eq 14
+             end
+         end
+         
 end
 end
 
@@ -155,4 +181,5 @@ def add_many_scores(count, pins)
     count.times do
         @game.add_score(pins)
     end
+end
 end
